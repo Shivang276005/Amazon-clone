ad533ac15a63777335f12f75db95fd25cd4b12e1
Shivang276005 <shivang.270605@gmail.com>
2026-03-03 21:14:31 +0530

    Add formatMoney utility and update price formatting in product and cart displays

--- javascript-amazon-project-main/script/amazon.js
+++ javascript-amazon-project-main/script/amazon.js
@@ -1,6 +1,7 @@
 import {cart,addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
-
+import { formatMoney } from './utils/money.js';
+formatMoney
 let productHTML = '';
 products.forEach((product)=>{
   productHTML += `
@@ -23,7 +24,7 @@ products.forEach((product)=>{
           </div>
 
           <div class="product-price">
-            $${(product.priceCents /100).toFixed(2)}
+            $${formatMoney(product.priceCents)}
           </div>
 
           <div class="product-quantity-container">
