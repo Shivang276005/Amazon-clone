3229a31b4e95f06384290dc714dcc5a46ab7056c
Shivang276005 <shivang.270605@gmail.com>
2026-03-20 01:36:30 +0530

    Changed the whole code to follow up the lecture.

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,56 +1,48 @@
 export let cart = JSON.parse(localStorage.getItem('cart'));
+
 if (!cart) {
   cart = [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2,
-    deliveryOptionId : '1'
-  },{
+  }, {
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1,
-    deliveryOptionId : '1'
-  }]
+    quantity: 1
+  }];
 }
-function renderCartItems(){
+
+function saveToStorage() {
   localStorage.setItem('cart', JSON.stringify(cart));
 }
 
-// helper to compute total number of items in the cart
-export function getCartQuantity() {
-  let quantity = 0;
-  cart.forEach((ci) => {
-    quantity += ci.quantity;
-  });
-  return quantity;
-}
-
-export function addToCart(productId){
+export function addToCart(productId) {
   let matchingItem;
-  cart.forEach((cartItem)=>{
-    if(productId === cartItem.productId){
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
       matchingItem = cartItem;
     }
   });
-  const quantitySelected = document.getElementById(`js-quantity-selector-${productId}`);
-  const quantity = Number(quantitySelected.value); 
+
   if (matchingItem) {
-    matchingItem.quantity+=quantity;
-  }else{
-    cart.push({ //{ variableName: variableName }
-        productId,
-        quantity,
-        deliveryOptionId : '1'
-      }
-  )}
-  renderCartItems();
-  console.log(cart);
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1
+    });
+  }
+
+  saveToStorage();
 }
-export function removeCartItem(productId) {
-  let newCart = [];
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
   cart.forEach((cartItem) => {
     if (cartItem.productId !== productId) {
       newCart.push(cartItem);
     }
   });
   cart = newCart;
-  renderCartItems();
+  saveToStorage();
 }
