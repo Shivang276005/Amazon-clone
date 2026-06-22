7dfc65c7c8c445714cd2dfb20ec7537f20b7830f
Shivang276005 <shivang.270605@gmail.com>
2026-06-22 18:00:04 +0530

    Re-visited the concepts of JavaScript and rewritten the code of several files to make it fully functional.

---
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -0,0 +1,85 @@
+import { cart, addToCart, calculateCartQuantity } from "../data/cart.js";
+import { products } from "../data/products.js";
+
+let productsHTML = '';
+products.forEach((product)=>{
+  productsHTML += `
+  <div class="product-container">
+    <div class="product-image-container">
+      <img class="product-image"
+        src=${product.image}>
+    </div>
+
+    <div class="product-name limit-text-to-2-lines">
+      ${product.name}
+    </div>
+
+    <div class="product-rating-container">
+      <img class="product-rating-stars"
+        src="images/ratings/rating-${product.rating.stars*10}.png">
+      <div class="product-rating-count link-primary">
+        ${product.rating.count}
+      </div>
+    </div>
+
+    <div class="product-price">
+      $${(product.priceCents/100).toFixed(2)}
+    </div>
+
+    <div class="product-quantity-container">
+      <select class = "js-quantity-selector-${product.id}">
+        <option selected value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+      </select>
+    </div>
+
+    <div class="product-spacer"></div>
+
+    <div class="added-to-cart js-added-${product.id}">
+      <img src="images/icons/checkmark.png">
+      Added
+    </div>
+
+    <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id = "${product.id}">
+      Add to Cart
+    </button>
+  </div>
+  `
+});
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+function updateCartQuantity(){
+  const cartQuantity = calculateCartQuantity();
+  if(cartQuantity == 0){
+    document.querySelector('.js-cart-quantity').innerHTML = '';
+  }else{
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+  }
+}
+updateCartQuantity();
+
+let intervalId;
+document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
+  button.addEventListener('click',()=>{
+    const productId = button.dataset.productId;
+    const selectedQuantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
+
+    addToCart(productId,selectedQuantity);
+ 
+    updateCartQuantity();
+   
+    clearInterval(intervalId);
+    document.querySelector(`.js-added-${productId}`).classList.add('display-added');
+    intervalId = setTimeout(() => {
+      document.querySelector(`.js-added-${productId}`).classList.remove('display-added');
+    }, 2000);
+  });
+});
